054977cefc9f141b32191e11e23ca4ca178c800c
Matthias Diener <matthias.diener@digitalrheumalab.de>
2025-10-28 15:46:54 +0100

    Setup of Pundit and Admin Rights to  create, view, edit and destroy Gigs as an admin

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -2,7 +2,49 @@ class GigsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @gigs = Gig.all.order(date: :desc)
+    @gigs = policy_scope(Gig)   # <-- required by Pundit
   end
-  
+
+  def show
+    @gig = Gig.find(params[:id])
+    authorize @gig              # <-- call policy show?
+  end
+
+  def new
+    @gig = Gig.new
+    authorize @gig              # <-- call policy new?/create?
+  end
+
+  def create
+    @gig = Gig.new(gig_params)
+    authorize @gig              # <-- must come BEFORE save
+    @gig.save!
+    redirect_to @gig
+  end
+
+  def edit
+    @gig = Gig.find(params[:id])
+    authorize @gig              # <-- update?
+  end
+
+  def update
+    @gig = Gig.find(params[:id])
+    authorize @gig
+    @gig.update!(gig_params)
+    redirect_to @gig
+  end
+
+  def destroy
+    @gig = Gig.find(params[:id])
+    authorize @gig
+    @gig.destroy
+    redirect_to gigs_path
+  end
+
+  private
+
+  def gig_params
+    params.require(:gig).permit(:title, :contact, :description, :source, :category, :date)
+  end
+
 end
