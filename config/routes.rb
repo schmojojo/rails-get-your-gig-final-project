12acbd77ea0360b9e13d157f0a75d6cb6fc9483c
Dumitru Baltatescu <dumitrubaltatescu.work@gmail.com>
2025-10-28 15:03:43 +0100

    updated navbar and styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
