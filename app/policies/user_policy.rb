cdb01ba847a3c214c4db68e1bf8f1712e9975276
Dumitru Baltatescu <dumitrubaltatescu.work@gmail.com>
2025-11-03 17:13:41 +0100

    added option to view all registered users on index only

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,4 +1,8 @@
 class UserPolicy < ApplicationPolicy
+  def index?
+    user&.admin?
+  end
+
   def show?
     user == record
   end
@@ -22,4 +26,15 @@ class UserPolicy < ApplicationPolicy
   def destroy?
     user == record
   end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      if user.admin?
+        scope.all
+       else
+         scope.none
+      end
+    end
+  end
+
 end
